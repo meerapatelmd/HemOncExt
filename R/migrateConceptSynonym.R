fbcfa32dbdb8b4804d40e88e331c82ba113769b6
Meera Patel <patelm9@mskcc.org>
2020-07-22 15:26:08 -0400

    add migrateConceptSynonym function

---
+++ R/migrateConceptSynonym.R
@@ -0,0 +1,43 @@
+#' Migrate HemOnc and RxNorm Vocabularies Synonyms
+#' @description This function executes on the condition that there are zero rows in the concept_ancestor table in the hemonc_extension schema.
+#' @import SqlRender
+#' @import pg13
+#' @import dplyr
+#' @param source_schema The schema where the main OMOP vocabularies are loaded.
+#' @export
+
+migrateConceptSynonym <-
+        function(conn,
+                 source_schema) {
+
+                tableName <- "concept_synonym"
+
+                # Get distinct row counts for the 'hemonc_extension.concept' table
+                extension_nrow <-
+                        pg13::query(conn = conn,
+                                    sql_statement = pg13::renderRowCount(distinct = TRUE,
+                                                                         schema = "hemonc_extension",
+                                                                         tableName = tableName))
+
+                # Nrow is 0, it means that the HemOnc and RxNorm concepts have not been migrated and will be migrated now
+
+                if (extension_nrow$count == 0) {
+
+                        base <- system.file(package = "hemOncExt")
+                        path <- paste0(base, "/sql/migrateConceptSynonym.sql")
+
+                        sql_statement <- SqlRender::render(SqlRender::readSql(path),
+                                                           schema = source_schema)
+
+                        source <-
+                                pg13::query(conn = conn,
+                                            sql_statement = sql_statement)
+
+
+                        pg13::appendTable(conn = conn,
+                                          schema = "hemonc_extension",
+                                          tableName = tableName,
+                                          .data = source)
+                }
+
+        }
---
+++ inst/sql/migrateConceptSynonym.sql
@@ -0,0 +1,18 @@
+WITH target_concepts AS (
+SELECT *
+FROM @schema.concept
+WHERE LOWER(vocabulary_id) IN  ('hemonc')
+UNION
+SELECT *
+FROM @schema.concept
+WHERE LOWER(vocabulary_id) IN ('rxnorm',
+                                'rxnorm extension')
+        AND LOWER(concept_class_id) IN ('ingredient',
+                                        'precise ingredient')
+)
+
+SELECT a.*
+FROM target_concepts c
+INNER JOIN @schema.concept_synonym a
+ON a.concept_id = c.concept_id
+;
