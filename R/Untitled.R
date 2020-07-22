0f4df09c6fd453eba9bf9e5a3e0d299e30980102
Meera Patel <patelm9@mskcc.org>
2020-07-22 15:52:39 -0400

    add constrainHemOncExtSchema function

--- R/Untitled.R
+++ R/Untitled.R
@@ -1,14 +1,10 @@
 # pg13::lsTables(conn = conn,
 #                schema = "hemonc_extension")
 
-conn <- chariot::connect_athena()
+#conn <- chariot::connect_athena()
 # pg13::send(conn = conn,
 #            sql_statement = "DROP SCHEMA hemonc_extension CASCADE")
 
 
-chariot::execute_athena_constraints()
+#chariot::execute_athena_constraints()
 
-
-
-download.file(url = "https://raw.githubusercontent.com/OHDSI/CommonDataModel/master/PostgreSQL/OMOP%20CDM%20postgresql%20constraints.txt",
-              destfile = "constraints.txt")
