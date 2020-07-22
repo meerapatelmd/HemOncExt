5e57f1a5215ebe34b7485db827c62b3fb9d6d4a9
Meera Patel <patelm9@mskcc.org>
2020-07-22 14:37:31 -0400

    bug fix migrateConceptAncestor function

--- inst/sql/migrateConceptAncestor.sql
+++ inst/sql/migrateConceptAncestor.sql
@@ -12,12 +12,12 @@ WHERE LOWER(vocabulary_id) IN ('rxnorm',
 )
 
 SELECT a.*
-FROM target_concepts
+FROM target_concepts c
 LEFT JOIN @schema.concept_ancestor a
-ON a.ancestor_concept_id = concept_id
+ON a.ancestor_concept_id = c.concept_id
 UNION
-SELECT a.*
+SELECT d.*
 FROM target_concepts
-LEFT JOIN @schema.concept_ancestor a
-ON a.descendant_concept_id = concept_id
+LEFT JOIN @schema.concept_ancestor d
+ON d.descendant_concept_id = c.concept_id
 ;
