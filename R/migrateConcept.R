59c0c51da9de04c488afb904c31ce59c023a6b4a
Meera Patel <patelm9@mskcc.org>
2020-07-22 14:24:50 -0400

    add migrateConcept function

--- R/migrateConcept.R
+++ R/migrateConcept.R
@@ -1,19 +1,38 @@
 #' Migrate HemOnc and RxNorm Vocabularies
+#' @description This function executes on the condition that there are zero rows in the concept table.
 #' @import SqlRender
+#' @import pg13
 #' @param source_schema The schema where the main OMOP vocabularies are loaded.
-
-
+#' @export
 
 migrateConcept <-
         function(conn,
                  source_schema) {
 
-                base <- system.file(package = "hemOncExt")
-                path <- paste0(base, "/sql/migrateConcept.sql")
+                # Get distinct row counts for the 'hemonc_extension.concept' table
+                extension_concept_nrow <-
+                        pg13::query(conn = conn,
+                                    sql_statement = pg13::renderRowCount(distinct = TRUE,
+                                                                         schema = "hemonc_extension",
+                                                                         tableName = "concept"))
+                # Nrow is 0, it means that the HemOnc and RxNorm concepts have not been migrated and will be migrated now
 
-                SqlRender::render(SqlRender::readSql(path),
-                                                   schema = source_schema)
+                if (extension_concept_nrow == 0) {
 
+                        base <- system.file(package = "hemOncExt")
+                        path <- paste0(base, "/sql/migrateConcept.sql")
 
+                        sql_statement <- SqlRender::render(SqlRender::readSql(path),
+                                                           schema = source_schema)
+
+                        source_concepts <-
+                                pg13::query(conn = conn,
+                                            sql_statement = sql_statement)
+
+                        pg13::appendTable(conn = conn,
+                                          schema = "hemonc_extension",
+                                          tableName = "concept",
+                                          .data = source_concepts)
+                }
 
         }
